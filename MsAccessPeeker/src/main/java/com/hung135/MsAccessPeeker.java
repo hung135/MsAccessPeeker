15bc36fdb8bf78792941ecb9bc9eb8eb9cc5dd04
Hung Nguyen <hung.nguyen@cfpb.gov>
2018-11-14 15:13:45 -0500

    checkin in code for the day

--- MsAccessPeeker/src/main/java/com/hung135/MsAccessPeeker.java
+++ MsAccessPeeker/src/main/java/com/hung135/MsAccessPeeker.java
@@ -3,11 +3,19 @@
  */
 package com.hung135;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -21,22 +29,57 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 /**
  * @author hung
  *
  */
 class utils {
-	public static void printFileInfo(String filename) throws IOException {
+	public static void writeLogFile(File fileName, List<String> rowData) throws IOException {
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName.toString(), true));
+		for (String row : rowData) {
+			writer.append(row);
+		}
+
+		writer.close();
+	}
+
+	public static void printFileInfo(String filename, File outLogFile) throws IOException {
 		HashCode hash = com.google.common.io.Files.hash(new File(filename), Hashing.md5());
-		System.out.println("\t"+ hash.toString());
+		List<String> row = new ArrayList<String>();
+
+	
 		
 		File f = new File(filename);
-		System.out.println("\tLastModified: "+ (new Date(f.lastModified())));
- 
+		System.out.println("\t" + hash.toString());
+		System.out.println("\tLastModified: " + (new Date(f.lastModified())));
 		
+		if (outLogFile.exists()) {
+			
+			if (outLogFile != null) {
+				row.add("File: " + filename);
+				row.add("Hash: " + hash.toString());
+				row.add("LastModified: " + (new Date(f.lastModified())));
+				writeLogFile(outLogFile, row);
+			}
+		}
+		else {
+			
+			row.add("File,Hash,LastModified");
+			
+			writeLogFile(outLogFile, row);
+			
+			
+		}
 	}
-	
-	public static List<String> getAccessFiles(List<String> dirs, int depth) throws InterruptedException {
+
+	public static List<String> getAccessFiles(List<String> dirs, int depth, File outLogFile)
+			throws InterruptedException {
 
 		List<String> fileList = new ArrayList<>();
 
@@ -46,17 +89,16 @@ class utils {
 		for (String directory : dirs) {
 			// System.out.println("Looking in :" + directory + "--------");
 			File folder = new File(directory);
-			if (directory.toString().equals("/Users/nguyenhu/Desktop/census/2010_AIANSF_MSAccessShell")) {
-				// System.out.println("ohhh");
 
-				// TimeUnit.SECONDS.sleep(5);
-			}
 			File[] files = folder.listFiles(new FileFilter() {
 				private final FileNameExtensionFilter filter = new FileNameExtensionFilter("MsAccess files", "mdb",
 						"accdb");
 
 				public boolean accept(File file) {
 					boolean x = true;
+					if (file.toString().contains("/.py/")) {
+						return false;
+					}
 					if (file.toString().contains("/hsqldb-2.4.1")) {
 						return false;
 					}
@@ -82,15 +124,15 @@ class utils {
 						System.out.println("Access File Found: \n\t" + i);
 
 						try {
-						utils.printFileInfo(i.toString());
-							
+							utils.printFileInfo(i.toString(), outLogFile);
+
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 
 					} else {
-						
+
 						subDirs.add(i.toString());
 						// System.out.println("Adding toRecurse: " + i.toString());
 
@@ -107,7 +149,7 @@ class utils {
 					aSubDir.add(aDir);
 					// System.out.println(subDirs.size() + "------size---Recursing\n\t\t" +
 					// subDirs.toString());
-					List<String> subFiles = getAccessFiles(aSubDir, depth + 1);
+					List<String> subFiles = getAccessFiles(aSubDir, depth + 1, outLogFile);
 
 					for (String i : subFiles) {
 						fileList.add(i.toString());
@@ -146,27 +188,67 @@ public class MsAccessPeeker {
 		}
 	}
 
+	public static void argParser(String[] args) throws InterruptedException {
+		ArgumentParser parser = ArgumentParsers.newFor("java -jar MsAccessPeeker.jar").build().defaultHelp(true)
+				.description("Finds all MsAccess files");
+		parser.addArgument("-hash", "--crc").choices("SHA-256", "SHA-512", "SHA1", "MD5", "None").setDefault("None")
+				.help("Specify hash function to run on file");
+		parser.addArgument("-o", "--outfile").nargs(1).help("Output file");
+		parser.addArgument("-d").nargs("*").help("Starting Directory");
+		Namespace ns = null;
+		try {
+			ns = parser.parseArgs(args);
+		} catch (ArgumentParserException e) {
+			parser.handleError(e);
+			System.exit(1);
+		}
+		// MessageDigest digest = null;
+
+		// System.out.println(ns.toString());
+		// System.out.print(ns.getString("d"));
+
+		/*
+		 * try { digest = MessageDigest.getInstance(ns.getString("crc")); } catch
+		 * (NoSuchAlgorithmException e) {
+		 * System.err.printf("Could not get instance of algorithm %s: %s",
+		 * ns.getString("crc"), e.getMessage()); System.exit(1); }
+		 */
+
+		File outFile = null;
+		for (String name : ns.<String>getList("outfile")) {
+			System.out.println(name);
+			outFile = new File(name);
+			
+		}
+
+		for (String name : ns.<String>getList("d")) {
+			Path path = Paths.get(name);
+			crawDriectory(path.toString(), outFile);
+
+		}
+	}
+
 	/**
 	 * @param args
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
+		argParser(args);
+
+	}
+
+	private static void crawDriectory(String dirPath, File outLogFile) throws InterruptedException {
 		List<String> dirs = new ArrayList<>();
-		Path currentRelativePath = Paths.get("");
+		Path currentRelativePath = Paths.get(dirPath);
 		String s = currentRelativePath.toAbsolutePath().toString();
- 	
-	 
-		
+		System.out.println("Crawling Directory: " + s);
 		dirs.add(s);
-		 
-
-		List<String> files = utils.getAccessFiles(dirs, 0);
+		utils.getAccessFiles(dirs, 0, outLogFile);
+		// List<String> files = utils.getAccessFiles(dirs, 0);
 		// print_columns(filePath);
-
-		for (String i : files) {
-			int ii = 1;
-			// System.out.println(i);
-		}
+		/*
+		 * for (String i : files) { int ii = 1; // System.out.println(i); }
+		 */
 
 	}
 
