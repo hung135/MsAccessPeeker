f71fd8b7172aaabce79c81f26bc267d4511c4784
hung <hung@mac2014.fios-router.home>
2018-11-13 22:07:00 -0500

    test checking

---
+++ MsAccessPeeker/src/main/java/com/hung135/MsAccessPeeker.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.hung135;
+
+/**
+ * @author hung
+ *
+ */
+class test{
+	int t=0;
+	test(int tt){
+		t=tt;
+	}
+	
+	
+}
+public class MsAccessPeeker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
